14fa17b0633dedbbb84cfea0824120999ab4ab62
Julia_Urbaniak <245953@edu.p.lodz.pl>
2024-05-08 17:34:02 +0200

    Constants

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUM = 12;
+    public static final int TOT = -1;
+    public static final int RET = 11;
+    private final int[] numbers = new int[NUM];
 
-    public int total = -1;
+    public int total = TOT;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOT;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == RET;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return TOT;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return TOT;
         return numbers[total--];
     }
 
