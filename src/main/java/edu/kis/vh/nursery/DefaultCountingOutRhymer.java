340c9c0b9e330eeb30f2010d3c6679988c5e8684
Aleksandra Imiolczyk <245818@edu.p.lodz.pl>
2024-05-09 14:55:02 +0200

    11.Hermetyzacja nieprywatnych pól

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,10 +2,34 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int num12 = 12;
-    private static final int numMinus1 = -1;
-    private static final int num11 = 11;
+    private static int num12 = 12;
+    private static int numMinus1 = -1;
+    private static int num11 = 11;
     private int[] numbers = new int[num12];
+
+    public static int getNum12() {
+        return num12;
+    }
+
+    public static void setNum12(int num12) {
+        DefaultCountingOutRhymer.num12 = num12;
+    }
+
+    public static int getNumMinus1() {
+        return numMinus1;
+    }
+
+    public static void setNumMinus1(int numMinus1) {
+        DefaultCountingOutRhymer.numMinus1 = numMinus1;
+    }
+
+    public static int getNum11() {
+        return num11;
+    }
+
+    public static void setNum11(int num11) {
+        DefaultCountingOutRhymer.num11 = num11;
+    }
 
     public int getTotal() {
         return total;
@@ -39,4 +63,15 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
