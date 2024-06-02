8c419c2122055ddf0a9f9f895621ae53b4938f73
Jakub <245762@edu.p.lodz.pl>
2024-06-02 20:52:37 +0200

    17. Wygenerowanie i uzupełnienie dokumentacji dla klasy DefaultCountingOutRhymer (oraz poprawa nazw zmiennych)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,37 +1,54 @@
 package edu.kis.vh.nursery;
 
+/**
+ *
+ */
 public class DefaultCountingOutRhymer {
 
-    public static final int num12 = 12;
-    public static final int numMinus1 = -1;
-    public static final int num11 = 11;
-    private int[] numbers = new int[num12];
+    public static final int size = 12;
+    public static final int is_empty = -1;
+    public static final int max_capacity = 11;
+    private int[] numbers = new int[size];
 
-    public int total = numMinus1;
+    public int total = is_empty;
 
+    /**
+     * @param in the value to be added to the stack
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-
+    /**
+     * @return true if stack is empty, false if not
+     */
     public boolean callCheck() {
-        return total == numMinus1;
+        return total == is_empty;
     }
 
+    /**
+     * @return true if stack is full, false if not
+     */
     public boolean isFull() {
-        return total == num11;
+        return total == max_capacity;
     }
 
+    /**
+     * @return the top value of the stack if it is not empty, otherwise is_empty
+     */
     protected int peekaboo() {
         if (callCheck())
-            return numMinus1;
+            return is_empty;
         return numbers[total];
     }
 
+    /**
+     * @return the top value of the stack if it is not empty, otherwise is_empty
+     */
     public int countOut() {
         if (callCheck())
-            return numMinus1;
+            return is_empty;
         return numbers[total--];
     }
 
